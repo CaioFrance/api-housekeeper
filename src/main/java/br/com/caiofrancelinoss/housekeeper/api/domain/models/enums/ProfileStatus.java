0d030312d0c20c07fe8447c8eedab815e8c62297
Caio Francelino <caio.francelinosena@gmail.com>
2023-06-19 21:16:19 -0400

    fix: deserialize enum when tests are running

--- src/main/java/br/com/caiofrancelinoss/housekeeper/api/domain/models/enums/ProfileStatus.java
+++ src/main/java/br/com/caiofrancelinoss/housekeeper/api/domain/models/enums/ProfileStatus.java
@@ -1,8 +1,28 @@
 package br.com.caiofrancelinoss.housekeeper.api.domain.models.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum ProfileStatus {
-    AVAILABLE,
-    BUSY,
-    UNAVAILABLE,
-    EXCLUDED
+    AVAILABLE("AVAILABLE"),
+    BUSY("BUSY"),
+    UNAVAILABLE("UNAVAILABLE"),
+    EXCLUDED("EXCLUDED");
+    
+    @JsonValue
+    private final String value;
+    
+    ProfileStatus(String value) {
+        this.value = value;
+    }
+    
+    @JsonCreator
+    public static ProfileStatus fromString(String value) {
+        for (ProfileStatus ps : ProfileStatus.values()) {
+            if (ps.value.equals(value)) {
+                return ps;
+            }
+        }
+        return null;
+    }
 }
